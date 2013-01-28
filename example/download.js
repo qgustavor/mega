fc9b9e22ee11af313b625c371e20a8659440f4ba
Tõnis Tiigi <tonistiigi@gmail.com>
2013-01-29 01:46:49 +0200

    Simple progress emitter

--- example/download.js
+++ example/download.js
@@ -14,6 +14,16 @@ mega.file(argv._[0]).loadAttributes(function(err, file) {
 
   var dl = file.download()
   dl.pipe(fs.createWriteStream(file.name), {end: false})
+
+  var _p
+  dl.on('progress', function (stats) {
+    var p = Math.round(stats.bytesLoaded / stats.bytesTotal * 100)
+    if (p != _p) {
+      console.log(p + '%')
+      _p = p
+    }
+  })
+
   dl.on('end', function() {
     console.log('Saved OK!')
   })
