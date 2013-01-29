87708f910d6ff5d61b128bf0a5f297d8d872fafa
Tõnis Tiigi <tonistiigi@gmail.com>
2013-01-30 00:07:02 +0200

    Downloading support for list example

--- example/list.js
+++ example/list.js
@@ -1,13 +1,32 @@
+var fs = require('fs')
 var mega = require('../lib/mega')
 var argv = require('optimist')
   .demand(2)
-  .usage('USAGE: node example/list <email> <password>')
+  .usage('USAGE: node example/list <email> <password>\n' +
+         '       node example/list <email> <password> --download pattern')
   .argv
 
 var storage = mega(argv._[0], argv._[1])
 
 storage.on('ready', function() {
   storage.root.forEach(function(f) {print(f)})
+
+  for (var id in storage.files) {
+    var f = storage.files[id]
+    if (!f.directory) {
+      if (f.name.indexOf(argv.download) != -1) {
+        (function(f) {
+        console.log('Downloading', f.name)
+        var dl = f.download()
+        dl.pipe(fs.createWriteStream(f.name), {end: false})
+        dl.on('end', function() {
+          console.log('Downloading', f.name, 'OK')
+        })
+        })(f)
+      }
+    }
+  }
+
 })
 
 function print(f, indent) {
