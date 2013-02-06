b5c158084fa6f47e2d8aba150d9b7eac90bbfd13
Tõnis Tiigi <tonistiigi@gmail.com>
2013-02-07 00:07:10 +0200

    Adding nice progress bar to download example

--- example/download.js
+++ example/download.js
@@ -1,4 +1,5 @@
 var fs = require('fs')
+var ProgressBar = require('progress')
 var argv = require('optimist')
   .demand(1)
   .usage('USAGE: node example/download http://mega.co.nz/#!link!key')
@@ -15,16 +16,16 @@ mega.file(argv._[0]).loadAttributes(function(err, file) {
   var dl = file.download()
   dl.pipe(fs.createWriteStream(file.name), {end: false})
 
-  var _p
+  var bar
   dl.on('progress', function (stats) {
-    var p = Math.round(stats.bytesLoaded / stats.bytesTotal * 100)
-    if (p != _p) {
-      console.log(p + '%')
-      _p = p
-    }
+    if (!bar) bar = new ProgressBar('downloading [:bar] :percent :etas', {
+      total: stats.bytesTotal,
+      width: 50
+    })
+    bar.tick(stats.bytesLoaded - bar.curr)
   })
 
   dl.on('end', function() {
-    console.log('Saved OK!')
+    console.log('\nSaved OK!')
   })
 })
