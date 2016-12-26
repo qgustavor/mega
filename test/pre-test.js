aad0618ef9863134044f145f19e9baf6d2d0f473
Gustavo Rodrigues <qgustavor@users.noreply.github.com>
2016-12-26 08:01:38 -0200

    Moved test .babelrc to test folder

--- test/pre-test.js
+++ test/pre-test.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const path = require('path')
 
-fs.accessSync(path.resolve(__dirname, '../.babelrc.test'))
+fs.accessSync(path.resolve(__dirname, '.babelrc.test'))
 fs.renameSync(path.resolve(__dirname, '../.babelrc'), path.resolve(__dirname, '../.babelrc.build'))
-fs.renameSync(path.resolve(__dirname, '../.babelrc.test'), path.resolve(__dirname, '../.babelrc'))
+fs.renameSync(path.resolve(__dirname, '.babelrc.test'), path.resolve(__dirname, '../.babelrc'))
