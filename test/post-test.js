4ba5000614f5f3473e8b686870f7b26c37b55e25
Gustavo Rodrigues <qgustavor@users.noreply.github.com>
2016-12-26 08:26:51 -0200

    Fixed tests

--- test/post-test.js
+++ test/post-test.js
@@ -4,6 +4,6 @@ const path = require('path')
 fs.access(path.resolve(__dirname, '../.babelrc.build'), err => {
   // if don't exists is because tests were already cleaned because those failed
   if (err) process.exit(1)
-  fs.renameSync(path.resolve(__dirname, '../.babelrc'), path.resolve(__dirname, '.babelrc.test'))
+  fs.renameSync(path.resolve(__dirname, '../.babelrc'), path.resolve(__dirname, '.babelrc'))
   fs.renameSync(path.resolve(__dirname, '../.babelrc.build'), path.resolve(__dirname, '../.babelrc'))
 })
