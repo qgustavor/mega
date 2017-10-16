687ef22e6287555580d18bb428ff5c22a48c4750
Gustavo Rodrigues <qgustavor@users.noreply.github.com>
2017-10-16 08:33:10 -0200

    Fix test issues
    Updated standard package
    
    Using cross-env instead of pre/post scripts following
    https://github.com/avajs/ava/issues/1146#issuecomment-273791687

--- test/post-test.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-const path = require('path')
-
-fs.access(path.resolve(__dirname, '../.babelrc.build'), err => {
-  // if don't exists is because tests were already cleaned because those failed
-  if (err) process.exit(1)
-  fs.renameSync(path.resolve(__dirname, '../.babelrc'), path.resolve(__dirname, '.babelrc'))
-  fs.renameSync(path.resolve(__dirname, '../.babelrc.build'), path.resolve(__dirname, '../.babelrc'))
-})
--- test/pre-test.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs')
-const path = require('path')
-
-fs.accessSync(path.resolve(__dirname, '.babelrc'))
-fs.renameSync(path.resolve(__dirname, '../.babelrc'), path.resolve(__dirname, '../.babelrc.build'))
-fs.renameSync(path.resolve(__dirname, '.babelrc'), path.resolve(__dirname, '../.babelrc'))
