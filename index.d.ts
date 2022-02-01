8c636c35059e8ae437c394e4c6f5a0a100612ad1
ChampionBuffalo1 <vidish7nov@gmail.com>
2022-02-01 22:08:30 +0530

    Finished API class and added @types/node & @types/node-fetch

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,20 @@
+/// <reference types="node" />
+import { Transform } from 'stream';
 import { EventEmitter } from 'events';
+import { Agent as HttpAgent } from 'http';
+import { Agent as HttpsAgent } from 'https';
+
+import AbortController from 'abort-controller'
+import type * as fetch from 'node-fetch';
 
 declare function megajs(options: megajs.StorageOpts, cb?: megajs.errorCb): megajs.Storage;
 
 declare namespace megajs {
+
+    export function megaEncrypt(key: Buffer, options?: megaCryptOpts): void | Transform;
+    export function megaDecrypt(key: Buffer, options?: megaCryptOpts): Transform;
+    export function megaVerify(key: Buffer): void | Transform;
+
     export class Storage extends EventEmitter {
         api: API;
         key: Buffer;
@@ -20,15 +32,15 @@ declare namespace megajs {
         mounts: ReadonlyArray<File>[];
         files: { [id in string]: MutableFile };
         RSAPrivateKey: (number | number[])[]; // tsc generated this        
-        static fromJSON(json: StorageJSON): Storage;
         constructor(options: StorageOpts, cb?: errorCb);
-        login(cb: (error: err, storage: this) => void): void;
-        reload(cb: (error: err, mount: ReadonlyArray<File>[], force?: boolean) => void): any; // "A required parameter cannot follow an optional parameter."
+        toJSON(): StorageJSON;
+        close(cb: () => void): void;
+        static fromJSON(json: StorageJSON): Storage;
         mkdir(opt: mkdirOpts | string, cb?: errorCb): void;
         upload(opt: any, buffer: any, cb: any): void;
-        close(cb: () => void): void;
+        login(cb: (error: err, storage: this) => void): void;
         getAccountInfo(cb: (error: err, account: any) => void): void;
-        toJSON(): StorageJSON;
+        reload(cb: (error: err, mount: ReadonlyArray<File>[], force?: boolean) => void): any; // "A required parameter cannot follow an optional parameter."
         on(event: 'add', listener: (File: MutableFile) => void): this;
         on(event: 'move', listener: (file: File, oldParent: File) => void): this;
         on(event: 'ready', listener: (storage: this) => void): this;
@@ -41,8 +53,23 @@ declare namespace megajs {
         once(event: 'delete', listener: (file: Readonly<File>) => void): this;
     }
     export class API extends EventEmitter {
-        constructor(keepalive: boolean, opts: APIOpts);
-
+        fetch: any;
+        gateway: string;
+        counterId: string;
+        userAgent: string;
+        keepalive: boolean;
+        httpAgent: HttpAgent;
+        httpsAgent: HttpsAgent;
+        sn?: AbortController;
+        static globalApi?: API;
+        static getGlobalApi(): API;
+        constructor(keepalive: boolean, opts?: APIOpts);
+        close(): void;
+        pull(sn: AbortController, retryno?: number): void;
+        wait(url: fetch.RequestInfo, sn: AbortController): void;
+        defaultFetch(url: fetch.RequestInfo, opts?: fetch.RequestInit): any;
+        // Not sure what is the type of response in callback
+        request(json: { [key in string]: any }, cb: (error: err, response?: any) => void, retryno?: number): void;
     }
 
     export class File extends EventEmitter {
@@ -51,6 +78,7 @@ declare namespace megajs {
     export class MutableFile extends File {
         constructor(opts: FileOpts, storage: Storage);
         mkdir(opts: mkdirOpts | string, cb?: errorCb): void;
+        upload(opts: uploadOpts | string, data?: any, cb?: any): void;
     }
     export class AES {
         key: Buffer;
@@ -65,12 +93,6 @@ declare namespace megajs {
     type StorageStatus = 'ready' | 'connecting' | 'closed';
     type errorCb = (error: err) => void;
     type err = Error | null;
-    interface mkdirOpts {
-        name: string;
-        attributes?: any; // For now
-        target?: MutableFile; // or File maybe?
-        key?: Buffer;
-    }
     interface StorageOpts {
         email: string;
         password: string;
@@ -86,12 +108,43 @@ declare namespace megajs {
         options: StorageOpts;
     }
     interface APIOpts {
-
+        gateway?: string;
+        fetch?: any;
     }
-
     interface FileOpts {
 
     }
+    interface accountInfo {
+        type: number; // Not sure of the type
+        spaceUsed: number;
+        spaceTotal: number;
+        downloadBandwidthUsed: number;
+        downloadBandwidthTotal: number;
+        sharedBandwidthUsed: number;
+        sharedBandwidthLimit: number;
+    }
+    interface mkdirOpts {
+        name: string;
+        attributes?: any; // For now
+        target?: MutableFile; // or File maybe?
+        key?: Buffer;
+    }
+    interface uploadOpts {
+        name: string;
+        key?: Buffer | string;
+        size?: number;
+        maxChunkSize?: number;
+        maxConnections?: number;
+        initialChunkSize?: number;
+        chunkSizeIncrement?: number;
+        previewImage?: Buffer | ReadableStream; // Not entirely sure about the ReadableStream as the type
+        thumbnailImage?: Buffer | ReadableStream;
+    }
+    interface megaCryptOpts {
+        start?: number;
+        disableVerification?: boolean;
+    }
+
 }
 
 
