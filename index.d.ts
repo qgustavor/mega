1f93edd0f9e34d8c55693335c748119987f96158
ChampionBuffalo1 <vidish7nov@gmail.com>
2022-02-01 19:56:09 +0530

    Added key property to AES & fixed error type

--- index.d.ts
+++ index.d.ts
@@ -3,32 +3,31 @@ import { EventEmitter } from 'events';
 declare function megajs(options: megajs.StorageOpts, cb?: megajs.errorCb): megajs.Storage;
 
 declare namespace megajs {
-
     export class Storage extends EventEmitter {
-        static fromJSON(json: StorageJSON): Storage;
-        constructor(options: StorageOpts, cb?: errorCb);
         api: API;
-        files: { [id in string]: MutableFile };
-        options: StorageOpts;
-        status: StorageStatus;
-        login(cb: (error: Error, storage: this) => void): void;
+        key: Buffer;
+        sid: string;
+        aes: AES;
         name: string;
         user: any;
         email: string;
-        key: Buffer;
-        aes: AES;
-        sid: string;
-        RSAPrivateKey: (number | number[])[]; // tsc generated this
-        reload(cb: (error: Error, mount: ReadonlyArray<File>[], force?: boolean) => void): any; // "A required parameter cannot follow an optional parameter."
-        mounts: ReadonlyArray<File>[];
         shareKeys: any;
+        options: StorageOpts;
+        status: StorageStatus;
         root: MutableFile;
         trash: MutableFile;
         inbox: MutableFile;
+        mounts: ReadonlyArray<File>[];
+        files: { [id in string]: MutableFile };
+        RSAPrivateKey: (number | number[])[]; // tsc generated this        
+        static fromJSON(json: StorageJSON): Storage;
+        constructor(options: StorageOpts, cb?: errorCb);
+        login(cb: (error: err, storage: this) => void): void;
+        reload(cb: (error: err, mount: ReadonlyArray<File>[], force?: boolean) => void): any; // "A required parameter cannot follow an optional parameter."
         mkdir(opt: mkdirOpts | string, cb?: errorCb): void;
-        upload(opt: any, buffer: any, cb: any): any;
-        close(cb: any): void;
-        getAccountInfo(cb: any): void;
+        upload(opt: any, buffer: any, cb: any): void;
+        close(cb: () => void): void;
+        getAccountInfo(cb: (error: err, account: any) => void): void;
         toJSON(): StorageJSON;
         on(event: 'add', listener: (File: MutableFile) => void): this;
         on(event: 'move', listener: (file: File, oldParent: File) => void): this;
@@ -54,6 +53,7 @@ declare namespace megajs {
         mkdir(opts: mkdirOpts | string, cb?: errorCb): void;
     }
     export class AES {
+        key: Buffer;
         constructor(key: Buffer);
         encryptCBC(buffer: Buffer): Buffer;
         decryptCBC(buffer: Buffer): Buffer;
@@ -63,7 +63,8 @@ declare namespace megajs {
     }
     // Interfaces & Types
     type StorageStatus = 'ready' | 'connecting' | 'closed';
-    type errorCb = (error: Error) => void;
+    type errorCb = (error: err) => void;
+    type err = Error | null;
     interface mkdirOpts {
         name: string;
         attributes?: any; // For now
